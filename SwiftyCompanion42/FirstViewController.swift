338b2d4f0c29d9a8c09ab36d1e29c01f6a68751e
Rida BIKITARAN <rbikitar@e2r13p10.42.fr>
2016-10-13 21:24:20 +0200

    modif

--- SwiftyCompanion42/FirstViewController.swift
+++ SwiftyCompanion42/FirstViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+var datasecond : NSDictionary?
+
 class FirstViewController: UIViewController {
     
     @IBOutlet weak var loginField: UITextField!
@@ -23,7 +25,7 @@ class FirstViewController: UIViewController {
         static let tokenurl = "https://api.intra.42.fr/oauth/token/info"
         static let usersurl = "https://api.intra.42.fr/v2/users"
     }
-    
+    var resultApi : NSDictionary?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,20 +50,10 @@ class FirstViewController: UIViewController {
         var iserror = ""
         let task = NSURLSession.sharedSession().dataTaskWithRequest(request){
             (data, response, error) in
-            print(response)
-            if let err = error {
+            //print(response)
+            if error != nil {
                 print(error)
                 iserror = "1"
-            }
-            else if let d = data {
-                do {
-                    if let dic : NSDictionary = try NSJSONSerialization.JSONObjectWithData(d, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary {
-                        print(dic)
-                    }
-                }
-                catch (let err){
-                    print(err)
-                }
             }
         }
         task.resume()
@@ -75,20 +67,24 @@ class FirstViewController: UIViewController {
     }
     
     override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        var iserror = ""
         if (apiData.token == "" || !isvalid()) {
+            let semaphore = dispatch_semaphore_create(0)
             let request = NSMutableURLRequest(URL: apiData.posturl!)
             request.HTTPMethod = "POST"
             request.HTTPBody = ("grant_type=client_credentials&client_id="+apiData.UID+"&client_secret="+apiData.SECRET).dataUsingEncoding(NSUTF8StringEncoding)
             let task = NSURLSession.sharedSession().dataTaskWithRequest(request){
-                (data, response, error) in
-                if let err = error {
+                (data, response, error) -> Void in
+                if error != nil {
                     print(error)
                 }
                 else if let d = data {
                     do {
                         if let dic : NSDictionary = try NSJSONSerialization.JSONObjectWithData(d, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary {
                             apiData.token = dic["access_token"] as! String
+                            print("token OK")
                             print(apiData.token)
+                            dispatch_semaphore_signal(semaphore)
                         }
                     }
                     catch (let err){
@@ -96,11 +92,63 @@ class FirstViewController: UIViewController {
                     }
                 }
             }
-            task.resume()
-            return false
+            [task.resume()]
+            dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
         }
-        else {
+        if (apiData.token != "" && isvalid()){
+            let semaphore = dispatch_semaphore_create(0)
+            var login : String
+            login = loginField.text!
+            let seturlrequest = apiData.usersurl+"/"+login+"?access_token="+apiData.token
+            let urlrequest = NSURL(string: seturlrequest)
+            let request = NSMutableURLRequest(URL: urlrequest!)
+            request.HTTPMethod = "GET"
+            
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request){
+                (data, response, error) in
+                //print(response)
+                if error != nil {
+                    print(error)
+                    iserror = "1"
+                    print("il y a une erreur")
+                }
+                else if let d = data {
+                    do {
+                        if let dic : NSDictionary = try NSJSONSerialization.JSONObjectWithData(d, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary {
+                            //print("normalement le dictionnaire devrait etre la")
+                            //print(dic)
+                            //print("fin du doc")
+                            dispatch_semaphore_signal(semaphore)
+                            self.resultApi = dic
+                            datasecond = dic
+                        }
+                    }
+                    catch (let err){
+                        print(err)
+                    }
+                }
+            }
+            [task.resume()]
+            dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+            //let secondview = storyboard?.instantiateViewControllerWithIdentifier("Profile") as! ProfileViewController
+            //print("avant set datafromapi")
+            //secondview.dataFromApi = self.resultApi!
+            //print(resultApi)
+            //print("ensuite secondview")
+            //print(secondview.dataFromApi)
+            //navigationController?.pushViewController(secondview, animated: true)
+        }
+        if (iserror == "1"){
+            return false}
+        else{
             return true
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "validSegue"){
+            let secondview = storyboard!.instantiateViewControllerWithIdentifier("Profile") as! ProfileViewController
+            secondview.dataFromApi = resultApi!
         }
     }
 }
--- SwiftyCompanion42/ProfileViewController.swift
+++ SwiftyCompanion42/ProfileViewController.swift
@@ -11,6 +11,13 @@ import UIKit
 class ProfileViewController: UIViewController {
     // MARK: Properties
     
+    var dataFromApi : NSDictionary?
+    
+    @IBOutlet weak var posteLabel: UILabel!
+    @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var mailLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
     @IBAction func returnButton(sender: UIBarButtonItem) {
         dismissViewControllerAnimated(true, completion: nil)
     }
@@ -19,20 +26,52 @@ class ProfileViewController: UIViewController {
     // MARK: functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        //let firstview = storyboard!.instantiateViewControllerWithIdentifier("start") as! FirstViewController
+        //self.dataFromApi = firstview.resultApi
+        //let res = dataFromApi
+        self.dataFromApi = datasecond
+        let urlImg = dataFromApi!["image_url"] as! String
+        if let url = NSURL(string: urlImg) {
+            if let data = NSData(contentsOfURL: url) {
+                ppImageView.image = UIImage(data: data)
+            }        
+        }
         ppImageView.layer.borderWidth = 1
         ppImageView.layer.masksToBounds = false
         ppImageView.layer.borderColor = UIColor.blackColor().CGColor
         ppImageView.layer.cornerRadius = ppImageView.frame.width/2
         ppImageView.clipsToBounds = true
+        posteLabel.text = dataFromApi!["location"] as? String
+        let test = dataFromApi!["cursus_users"] as! NSMutableArray
+        //print(test)
+        print(test[0]["level"])
+        //levelLabel.text = dataFromApi!["cursus_users"]!["level"] as? Int
+        //var dict : NSDictionary = dataFromApi!["cursus_users"] as! NSDictionary
+        //if var dict = dataFromApi!["cursus_users"] as! NSDictionary
+        //{
+          //  let value = dict["level"] as! String
+           // levelLabel.text = value
+        //}
+//        print(toto)
+        let truc : Double = test[0]["level"] as! Double
+        levelLabel.text = truc.description
+        print(levelLabel.text)
+        loginLabel.text = dataFromApi!["login"] as? String
+        mailLabel.text = dataFromApi!["email"] as? String
+        phoneLabel.text = dataFromApi!["phone"] as? String
+        
+        print("debut res")
+        //print(dataFromApi)
+        print("fin res")
         // Do any additional setup after loading the view.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
     // MARK: - Navigation
 
